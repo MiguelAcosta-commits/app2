95f65c57c0dbb4e3b3e8918c6bb4ccf5ade58214
MiguelAcosta-commits <miguel_micky55@hotmail.com>
2019-12-28 19:23:57 -0600

    funcionamiento offline

--- src/pages/resultados/resultados.ts
+++ src/pages/resultados/resultados.ts
@@ -1,76 +1,144 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams, AlertController, Platform } from 'ionic-angular';
 // import { BuscadorProvider } from '../../providers/buscador/buscador';
 import { HuespedPage } from '../../pages/huesped/huesped';
-import { Storage } from '@ionic/storage';
-import { Network } from '@ionic-native/network';
 import { TodosProvider } from '../../providers/todos/todos';
+import { Storage } from '@ionic/storage';
 
 
 @Component({
     selector: 'page-resultados',
     templateUrl: 'resultados.html',
 })
-export class ResultadosPage {
+export class ResultadosPage implements OnInit {
 
     huespedPage = HuespedPage;
+
 
     // Funcionaria como base de datos local
     resultado: any = [];
     // Esta variable se cambiaria en el html de la vista en ves de resultados
     arrayHuesped: any = [];
+    isConnected = false;
+    Counter = 1;
+    limit = 1
 
+    ngOnInit() {
+           this.CargarDataTiming()
+    }
 
     constructor(public navCtrl: NavController,
             public navParams: NavParams,
             public _hs: TodosProvider,
+            private alertCtrl: AlertController,
             private platform: Platform,
-            private storage: Storage,
-            private network: Network) {
-              this.EstadoDeRed();
-
+            private storage: Storage) {
         }
-        // Ciclo de vida que se inicializa cuando se entra a una pagina
-    ionViewDidLoad() {
 
-        // Descomentar Cuando se este haciendo pruebas en el navegador
-        // Comentar las lineas cuando se va a ejecuatar en android apk
-        // this._hs.obtener_huespedes()
-        //     .subscribe((data: any) => {
-        //         this.resultado = data
-        //         this.guardarStorage();
-        //         console.log("entra a ionviewdidload")
-        //     })
 
-        // Comentar Cuando se este haciendo pruebas en el navegador
-        // Descomenatr las lineas cuando se va a ejecuatar en android apk
 
-        // // // Detecta si se ha desconectado de internet
-
+    public alert(mensaje: string){
+        const alert = this.alertCtrl.create({
+            title: 'Alerta de wifi!',
+            subTitle: mensaje,
+            buttons: ['OK']
+        });
+        alert.present();
     }
 
-    EstadoDeRed()
-    {
-      console.log("Ingreso a la funcion de red");
-      this.network.onDisconnect().subscribe(() => {
-        this.obtenerStorage()
-        console.log("Desconectado de Internet");
-    });
+    public CargarData() {
+    this._hs.getNetworkStatus().subscribe((connected: boolean) => {
+        this.isConnected = connected;
+        if (this.isConnected && this.Counter <= this.limit) {
+            try {
+                this._hs.obtenerHuesped().subscribe(
+                success => {
+                    this.resultado = success;
+                    this.guardarStorage()
+                    //this.alert('Hay internet android')
+                }, error => {
+                    this.obtenerStorage()
+                    //this.alert('No tiene internet android '+ this.resultado)
+                    console.log(JSON.stringify(error))
+                });
 
-  // // // Detecta si se ha conectado a internet
-  console.log("Conectado a Internet");
-    this.network.onConnect().subscribe(() => {
-        this._hs.obtener_huespedes()
-            .subscribe((data: any) => {
-                this.resultado = data
-                this.guardarStorage();
+            } catch (err) {
+                console.log('error en el servicio', err);
+                this.obtenerStorage()
+                this.alert('No tiene internet '+ this.resultado)
+            }
+
+        } else if(!this.isConnected && this.Counter <= this.limit){
+            try {
+                this._hs.obtenerHuesped().subscribe(
+                success => {
+                    this.resultado = success;
+                    this.guardarStorage()
+                    //his.alert('Hay internet')
+                }, error => {
+                    this.obtenerStorage()
+                    //this.alert('No tiene internet '+ this.resultado)
+                    console.log(JSON.stringify(error))
+                });
+
+            } catch (err) {
+                console.log('err testNetworkConnection', err);
+                this.obtenerStorage()
+                this.alert('No tiene internet '+ this.resultado)
+            }
+        }
+        this.Counter++;
+    })
+    }
+
+    public CargarDataTiming(){
+        setInterval( () =>{
+            this.CargarData()
+        })
+    }
+
+    public guardarStorage() {
+        if (this.platform.is('cordova')) {
+            //Celular
+            this.storage.ready().then( () =>{
+              this.storage.set('huesped', JSON.stringify(this.resultado));
             })
-    });
+        } else {
+            localStorage.setItem('huesped', JSON.stringify(this.resultado));
+        }
     }
 
+    public obtenerStorage() {
+        if (this.platform.is('cordova')) {
+          //Celular
+          this.storage.get('huesped').then((data) => {
+              this.resultado = JSON.parse(data);
+          })
+        } else {
+            //Escritorio
+            this.resultado = JSON.parse(localStorage.getItem("huesped"));
+        }
 
+    }
 
-    array: any = {
+    public buscar_huesped(ev: any) {
+        // CAMBIAR EN huesped.title por el que se tenia anterior
+        let valor = ev.target.value.toLowerCase();
+        this.arrayHuesped = [];
+        console.log(valor);
+        if (valor != '') {
+            for (let huesped of this.resultado) {
+                let nombre = huesped.h_nom.toLowerCase();
+                if (nombre.indexOf(valor) >= 0) {
+                    this.arrayHuesped.push(huesped);
+                }
+            }
+        } else {
+            this.arrayHuesped = [];
+        }
+    }
+
+    public arrayColores: any = {
         //Mayan Palace Golf
         'MST': 'mayangolf',
         'MMR': 'mayangolf',
@@ -88,59 +156,5 @@ export class ResultadosPage {
         'SMR': 'seagarden',
         'SSS': 'seagarden',
     }
-
-
-    buscar_huesped(ev: any) {
-
-        let valor = ev.target.value.toLowerCase();
-        this.arrayHuesped = [];
-        console.log(valor);
-        if (valor != '') {
-            for (let huesped of this.resultado) {
-                let nombre = huesped.h_nom.toLowerCase();
-                if (nombre.indexOf(valor) >= 0) {
-                    this.arrayHuesped.push(huesped);
-                }
-            }
-        } else {
-            this.arrayHuesped = [];
-        }
-    }
-
-    obtenerStorage() {
-
-      if (this.platform.is('cordova')) {
-        //Celular
-
-
-        this.storage.get('huesped').then((data) => {
-            this.resultado = JSON.parse(data);
-        })
-
-    } else {
-        //Escritorio
-        this.resultado = JSON.parse(localStorage.getItem("huesped"));
-    }
-
-    }
-
-    guardarStorage() {
-        if (this.platform.is('cordova')) {
-            //Celular
-
-            this.storage.ready()
-            .then(()=>
-            {
-              this.storage.set('huesped', JSON.stringify(this.resultado));
-            })
-
-
-        } else {
-            //Escritorio
-            localStorage.setItem('huesped', JSON.stringify(this.resultado));
-        }
-
-    }
-
 
 }
--- src/providers/todos/todos.ts
+++ src/providers/todos/todos.ts
@@ -2,25 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { URL_SERVICIOS } from '../../config/url.servicios';
 
-import { Storage } from '@ionic/storage';
 import { Platform } from 'ionic-angular';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Network } from '@ionic-native/network';
 
 @Injectable()
 export class TodosProvider {
 
 
-  constructor(public http: HttpClient, private platform:Platform, public storage:Storage,
-              public sqlite:SQLite)
-              {
-    console.log('Hello TodosProvider Provider');
-  }
-  obtener_huespedes()
-{
-  let url1 = URL_SERVICIOS + "/prueba/obtener_todos_los_huespedes/"
-  return this.http.get(url1)
+    private online: Observable<boolean> = null;
+    private hasConnection = new BehaviorSubject(false);
+
+    constructor(
+        private network: Network,
+        private platform: Platform,
+        private http: HttpClient) {
+
+        if (this.platform.is('cordova')) {
+            // on Device
+            this.network.onConnect().subscribe(() => {
+                console.log('El internet se ha conectado');
+                this.hasConnection.next(true);
+                return;
+            });
+            this.network.onDisconnect().subscribe(() => {
+                console.log('Se ha desconectado');
+                this.hasConnection.next(false);
+                return;
+            });
+        }
+    }
+
+    public getNetworkType(): string {
+        return this.network.type;
+    }
+
+    public getNetworkStatus(): Observable<boolean> {
+        return this.hasConnection.asObservable();
+    }
+
+    public obtenerHuesped(): Observable<any> {
+        return this.http.get(URL_SERVICIOS + "/prueba/obtener_todos_los_huespedes/");
+    }
+
 }
 
-
-
-}
